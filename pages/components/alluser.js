f68cc985ff5f799a125ebc33143515c068d16ab5
akram <s300m22@gmail.com>
2021-04-18 19:06:24 +0100

    last

--- pages/components/alluser.js
+++ pages/components/alluser.js
@@ -14,25 +14,73 @@ export default function alluser({posts}) {
 const result = posts.map(post => {
   return(
 
-    <div className="card col-12 col-md-3" key={post.id}>
-    <img  className="card-img-top" src={"https://source.unsplash.com/720x400/?" + post.id}  />
-    <div className="card-body">
-      <h5 className="card-title">{post.title}</h5>
-      <p className="card-text">{post.body}</p>
-      <Link  href={"blog/" + post.id} >
-      <a className="btn btn-primary">More</a>
-      </Link>
-    </div>
-</div>
+          <tr key={post.id}>
+
+             <td>{post.username}</td>
+             <td>{post.firstname + post.lastname}</td>
+             <td>{post.email}</td>
+             <td>{post.role}</td>
+                                  
+              <td>
+                  <div class="status-toggle">
+                    <input type="checkbox" id="status_1" class="check" />
+                    <label for="status_1" class="checktoggle">checkbox</label>
+                  </div>
+              </td>
+          </tr>
   )
 })
   return (
-    <div>
-
-            
-<div className='row'>{result}</div>
-                
-      Home 
+          <div>
+              <div class="page-wrapper">
+                <div class="content container-fluid">
+				
+					
+                  <div class="page-header">
+                    <div class="row">
+                      <div class="col-sm-12">
+                        <h3 class="page-title">Users</h3>
+                        <ul class="breadcrumb">
+                          <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                          <li class="breadcrumb-item active">Users</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+				
+					<div class="row">
+						<div class="col-md-12">
+						
+						
+							<div class="card">
+								<div class="card-body">
+									<div class="table-responsive">
+										<table class="datatable table table-hover table-center mb-0">
+											<thead>
+												<tr>
+													<th>Username</th>
+													<th>Name</th>
+													<th>Patient Name</th>
+													<th>Email</th>
+                                                    <th>Role</th>
+													<th>Status</th>
+													
+												</tr>
+											</thead>
+											<tbody>
+												{result}
+											
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						
+							
+						</div>
+					</div>
+				</div>			
+			</div>
     </div>
   )
 }
