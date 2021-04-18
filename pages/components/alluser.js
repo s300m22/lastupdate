a3b1fa406965ca806ceff9040425eaa8cb9942f5
akram <s300m22@gmail.com>
2021-04-18 01:08:57 +0100

    m8824

--- pages/components/alluser.js
+++ pages/components/alluser.js
@@ -1,6 +1,42 @@
 import React from 'react'
 
 const alluser = () => {
+
+	export const getStaticProps = async () => {
+		const res = await fetch('http://ffapps.itvip.live/api/auth/me')
+		const data = await res.json();
+		return {
+		  // props:{data}
+		  props: {posts:data}
+		  
+		}
+	  }
+	  
+	  export default function Home({posts}) {
+		const result = posts.map(post => {
+		  return(
+		
+			<div className="card col-12 col-md-3" key={post.id}>
+			<img  className="card-img-top" src={"https://source.unsplash.com/720x400/?" + post.id}  />
+			<div className="card-body">
+			  <h5 className="card-title">{post.usrname}</h5>
+			  <p className="card-text">{post.firstname}</p>
+			  <Link  href={"blog/" + post.id} >
+			  <a className="btn btn-primary">More</a>
+			  </Link>
+			</div>
+		</div>
+		  )
+		})
+		  return (
+			<div className={styles.container}>
+		
+		<div className='row'>{result}</div>
+						
+			  Home 
+			</div>
+		  )
+		}
     return (
         <div>
               <div class="page-wrapper">
